c2aa49361c2a7a0f8e6b816eee6030d632a5d565
Steven Schram <sbschram@gmail.com>
2026-05-26 22:21:40 -0400

    Update privacy policy for App Review.

--- js/components.js
+++ js/components.js
@@ -1,7 +1,7 @@
 // Centralized Component Loader
 // Eliminates duplicate header/footer loading code across all pages
 // Bump DEPLOY_VERSION on each deploy so footer/header cache bust
-const DEPLOY_VERSION = '20260526130000';
+const DEPLOY_VERSION = '20260527022101';
 
 class ComponentLoader {
   constructor() {
